9c01bdff874d1728884cc039bb7bacde10153873
Petri Sandström <petri.sandstrom@flockler.com>
2018-02-28 17:38:17 +0200

    feat: Improve review list semantics

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -99,6 +99,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
   const title = document.createElement('h2');
+  title.id = 'reviews__heading'
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
