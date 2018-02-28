b2f27194112e6307a68d837833102446b286ccb8
Petri Sandström <petri.sandstrom@flockler.com>
2018-02-28 09:40:39 +0200

    feat: Add service worker static caching

---
+++ js/index.js
@@ -0,0 +1,11 @@
+(function () {
+  if (!navigator.serviceWorker) return;
+
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    if (!navigator.serviceWorker.controller) {
+      return;
+    }
+  }).catch(function(error) {
+    console.log('Registration failed with ' + error);
+  });
+})();
