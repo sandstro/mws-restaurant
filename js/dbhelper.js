424e84cbbee0cd01d24693af9001367723b8bf1c
Petri Sandström <petri.sandstrom@flockler.com>
2018-03-10 16:22:25 +0200

    feat: Use Fetch API instead of xhr

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,27 +8,26 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
+    fetch(DBHelper.DATABASE_URL).then(response => {
+      if (response.ok) {
+        return response.json();
       }
-    };
-    xhr.send();
+      throw new Error('Network response was not ok.');
+    }).then(json => {
+      const restaurants = json;
+      callback(null, restaurants);
+    }).catch(err => {
+      const error = (`Request failed. Returned status of ${err.message}`);
+      callback(error, null);
+    });
   }
 
   /**
